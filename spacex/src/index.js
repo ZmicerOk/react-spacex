a9ab2fe864d228cd7e154e2bd621605beeef8ca4
ZmicerOk <vasikou73@gmail.com>
2020-10-13 22:43:35 +0300

    feat: add header to App

--- spacex/src/index.js
+++ spacex/src/index.js
@@ -6,6 +6,8 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
+  ()=>console.log('test ReactDOM.render cb')
+  
 );
 
