3dac4fb09082794fa63ef812ddf1d627e733c2bc
ZmicerOk <vasikou73@gmail.com>
2020-10-16 18:26:25 +0300

    feat: add Link to Details

--- spacex/src/components/Calendar/Calendar.jsx
+++ spacex/src/components/Calendar/Calendar.jsx
@@ -6,32 +6,39 @@ import { Link } from 'react-router-dom';
 const fetchData = new FetchData();
 const Calendar = () => {
   const [data, setData] = useState([]);
+  // const [id, setId] = useState(0);
   useEffect(() => {
     fetchData.getLaunches().then((launches) => setData(launches));
   }, []);
 
-  // console.log(data);
+  // console.log(id);
   return (
     <>
       <Main />
       <section className="calendar">
         <div className="container">
+          {/* <h3>{id}</h3> */}
           <ul className="calendar-list">
-            {data.map((el) => (
-              <li className="calendar-item" key={el.id}>
-                <article className="launches">
-                  <div className="launches-image">
-                    <img src={el.links.patch.small} alt={el.name} />
-                  </div>
-                  <div className="launches-content">
-                    <h2 className="launches-title">{el.name}</h2>
-                    <Link to="/details" className="button launches-details">
-                      Подробнее
-                    </Link>
-                  </div>
-                </article>
-              </li>
-            ))}
+            {data.map((el) => {
+              return (
+                <li className="calendar-item" key={el.id}>
+                  <article className="launches">
+                    <div className="launches-image">
+                      <img src={el.links.patch.small} alt={el.name} />
+                    </div>
+                    <div className="launches-content">
+                      <h2 className="launches-title">{el.name}</h2>
+                      <Link
+                        to="/details"
+                        // onClick={() => setId(el.id)}
+                        className="button launches-details">
+                        Подробнее
+                      </Link>
+                    </div>
+                  </article>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </section>
--- spacex/src/components/Details/Details.jsx
+++ spacex/src/components/Details/Details.jsx
@@ -1,37 +1,41 @@
 import React from 'react';
 import Main from '../Main/Main';
+import { Link } from 'react-router-dom';
 import './details.css';
-
-const Details = () => (
-  <>
-  <Main/>
-    <main className="details">
-      <div className="container">
-        <div className="details-row">
-          <div className="details-image">
-            <img src="https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png" alt="" />
+const Details = () => {
+  return (
+    <>
+      <Main />
+      <main className="details">
+        <div className="container">
+          <div className="details-row">
+            <div className="details-image">
+              <img src="https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png" alt="" />
+            </div>
+            <div className="details-content">
+              <p className="details-description">
+                Engine failure at 33 seconds and loss of vehicle
+              </p>
+            </div>
           </div>
-          <div className="details-content">
-            <p className="details-description">Engine failure at 33 seconds and loss of vehicle</p>
+          <div>
+            <iframe
+              className="details-youtube"
+              title="details-youtube"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/dLQ2tZEH6G0"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen></iframe>
           </div>
         </div>
-        <div>
-          <iframe
-            className="details-youtube"
-            title="details-youtube"
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/dLQ2tZEH6G0"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen></iframe>
-        </div>
-      </div>
-      <a href="calendar.html" className="button button-back">
-        go back
-      </a>
-    </main>
-  </>
-);
+        <Link to="/calendar" className="button button-back">
+          go back
+        </Link>
+      </main>
+    </>
+  );
+};
 
 export default Details;
